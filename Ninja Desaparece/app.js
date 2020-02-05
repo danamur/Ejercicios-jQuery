e0ecac6c36f29d817f771bcf612a502577c5731f
danamur <59931318+danamur@users.noreply.github.com>
2020-02-05 13:04:30 -0300

    Ejercicio de jQuery
    Desaparición de ninjas y aparición por medio de un botón.

---
+++ Ninja Desaparece/app.js
@@ -0,0 +1,30 @@
+$(document).ready(function () {  
+    var resultado = $('img');
+    $('.img1').click(function(){
+        $('#img1').fadeOut();
+    });
+    $('.img2').click(function(){
+        $('#img2').fadeOut();
+    });
+    $('.img3').click(function(){
+        $('#img3').fadeOut();
+    });
+    $('.img4').click(function(){
+        $('#img4').fadeOut();;
+    });
+    $('.img5').click(function(){
+        $('#img5').fadeOut();
+    });
+    $('.img6').click(function(){
+        $('#img6').fadeOut();
+    });
+    $('.img7').click(function(){
+        $('#img7').fadeOut();
+    });
+    $('.img8').click(function(){
+        $('#img8').fadeOut();;
+    });
+    $('.btn-dark').click(function(){
+        resultado.fadeIn(3000);
+    });
+});
